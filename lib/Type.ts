129851f6382a1098664ba324da56ec6e5887cf42
Samuel Schepp <sa.schepp@googlemail.com>
2020-11-24 13:34:06 +0100

    # eslint workaround

--- lib/Type.ts
+++ lib/Type.ts
@@ -2,6 +2,7 @@
  * An enum that represents the various types of a JSON object.
  * Undefined and null are treated equally.
  */
+// eslint-disable-next-line no-shadow
 export enum Type {
   string = "string",
   number = "number",
